5b85e25712c5caff6cd08624ca3df98be92a3ec4
2ood <kyungmin.official0@gmail.com>
2023-06-12 08:45:00 +0900

    hotfix(deploy) : changed run script

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 const BASEPATH_DUMMY_API="/api";
 const router_what_should_i_buy = require('./api/what-should-i-buy');
 
 app.use(BASEPATH_DUMMY_API+"/what-should-i-buy",router_what_should_i_buy);
 
-app.listen(process.env.PORT || 3000);
+app.listen(port,() => {
+    console.log(`Example app listening on ${port}`);
+});
