8881cbdac87ca2f61495af5e284a45737a0ff9c0
2ood <kyungmin.official0@gmail.com>
2023-06-12 08:35:31 +0900

    hotfix(port) : changed port to process-assigned port

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 const BASEPATH_DUMMY_API="/api";
 const router_what_should_i_buy = require('./api/what-should-i-buy');
 
 app.use(BASEPATH_DUMMY_API+"/what-should-i-buy",router_what_should_i_buy);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+app.listen(process.env.PORT || 3000);
